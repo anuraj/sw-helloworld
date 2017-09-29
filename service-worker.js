cc83747a9f261fe2047aed506146f042983a28ac
Anuraj <anuraj.p@msn.com>
2017-09-29 10:53:08 +0530

    More and More Updates

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,18 @@
 const messaging = firebase.messaging();
 
+messaging.onMessage(function (payload) {
+    console.log("Message received. ", payload);
+});
+
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+        body: 'Background Message body.',
+        icon: '/firebase-logo.png'
+    };
+
+    return self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
+
