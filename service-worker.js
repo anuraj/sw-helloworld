83f6179b517cbadb91251f06da8ccb8e8f3e7ed1
Anuraj <anuraj.p@msn.com>
2017-09-29 11:02:38 +0530

    Updated SW

--- service-worker.js
+++ service-worker.js
@@ -8,10 +8,6 @@ firebase.initializeApp(config);
 
 const messaging = firebase.messaging();
 
-messaging.onMessage(function (payload) {
-    console.log("Message received. ", payload);
-});
-
 messaging.setBackgroundMessageHandler(function (payload) {
     console.log('[firebase-messaging-sw.js] Received background message ', payload);
     const notificationTitle = 'Background Message Title';
