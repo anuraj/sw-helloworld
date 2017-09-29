e5d4f51ee17040f2d1bcc6c48531fbc7c3725e3a
Anuraj <anuraj.p@msn.com>
2017-09-29 10:56:59 +0530

    SW Fixes

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,14 @@
+importScripts('https://www.gstatic.com/firebasejs/4.4.0/firebase.js');
+var config = {
+    apiKey: "AIzaSyD726Qm78aJA1VCJyP4x-wjQZ2kTtJt2KE",
+    authDomain: "webviewdemoapp.firebaseapp.com",
+    databaseURL: "https://webviewdemoapp.firebaseio.com",
+    projectId: "webviewdemoapp",
+    storageBucket: "webviewdemoapp.appspot.com",
+    messagingSenderId: "321043996130"
+};
+firebase.initializeApp(config);
+
 const messaging = firebase.messaging();
 
 messaging.onMessage(function (payload) {
